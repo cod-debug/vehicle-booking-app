22cb0babd5d6e912ddaf33992db751eff250b353
unknown <roy.duenas.sdtpnoli@gmail.com>
2023-01-18 19:26:17 +0800

    updates

--- admin/admin-add-vehicle.php
+++ admin/admin-add-vehicle.php
@@ -145,13 +145,6 @@
                 <option>SUV</option>
                 <option>Van</option>
               </datalist>
-              <!-- <select class="form-control" name="v_category" id="exampleFormControlSelect1">
-                <option>Bus</option>
-                <option>Sedan</option>
-                <option>SUV</option>
-                <option>Van</option>
-
-              </select> -->
             </div>
 
             <div class="form-group col-md-6" >
