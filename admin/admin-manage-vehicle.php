2e472817e1f384017c780d1c061d720e3cde013b
cod-debug <roy.duenas@mobilemoney.ph>
2023-02-15 22:45:17 +0800

    udpates

--- admin/admin-manage-vehicle.php
+++ admin/admin-manage-vehicle.php
@@ -94,7 +94,7 @@
                 </thead>
                 <?php
 
-                    $ret="SELECT * FROM tms_vehicle "; 
+                    $ret="SELECT * FROM `tms_vehicle` INNER JOIN `tms_admin` WHERE `tms_vehicle`.`lessor_id` = `tms_admin`.`a_id` AND `tms_vehicle`.`lessor_id` = $aid"; 
                     $stmt= $mysqli->prepare($ret) ;
                     $stmt->execute() ;//ok
                     $res=$stmt->get_result();
